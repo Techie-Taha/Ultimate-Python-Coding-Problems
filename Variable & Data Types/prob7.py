5ccb12c38264b0dab13c73ec4ef58062bda7acb7
Taha <ta753090@gmail.com>
2025-08-22 00:51:48 -0400

    File added

---
+++ Variable & Data Types/prob7.py
@@ -0,0 +1,5 @@
+# ask the user 2 question, person's name and favorite color then print the statement like this Taha likes black color
+
+question_1 = input("What's your name? ")
+question_2 = input("What's your favorite color? ")
+print(question_1, "likes" , question_2, "color") # we could also apply formatted string
